f965fb53f72f1317e8336df3208df453c1b37583
Egor Chernyshev  22-ivt-3 <egorcher152@yandex.ru>
2024-12-19 18:31:49 +0300

    back

---
+++ back/BookStore.Core/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace BookStore.Core.Models
+{
+    public class Book
+    {
+        public const int MAX_TITLE_LENGTH = 250;
+
+        private Book(Guid id, string title, string author, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+
+        public Guid Id { get; }
+        public string Title { get; } = string.Empty;
+        public string Author { get; } = string.Empty;
+        public decimal Price { get; }
+
+        public static (Book Book, string Error) Create(Guid id, string title, string author, decimal price)
+        {
+            var error = string.Empty;
+
+            if(string.IsNullOrEmpty(title) || title.Length > MAX_TITLE_LENGTH)
+            {
+                error = "Название не должно быть больше 250 символов";
+            }
+
+            var book = new Book(id, title, author, price);
+
+            return (book, error);
+        }
+    }
+}
